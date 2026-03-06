baba1776fad2a3988e2db1060aab95cdfb9059c1
Parteek Marve <parteek.marve09@gmail.com>
2026-03-06 21:32:53 +0530

    new strings

---
+++ STRINGS.py
@@ -0,0 +1,12 @@
+# STRING
+string = "Parteek"
+print(string)
+first_name = "Parteek"
+last_name = "Marve"
+full_name = first_name + last_name
+print(full_name)
+
+long_dash = "-"*30 # prints dash 30 times
+print(long_dash)
+
+print(len(full_name))
---
+++ print.py
@@ -0,0 +1,6 @@
+#import requests
+
+# Downlaod A Web Page
+#response = requests.get("https://api.github.com")
+#print(response.status_code) # should show 200
+name = "Alice"
